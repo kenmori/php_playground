a47518c6e2f10296f34d43e2221661f05ab6c1a7
kenji <kennjimoritata@yahoo.co.jp>
2016-05-21 09:47:23 +0900

    add array

--- playground/message.php
+++ playground/message.php
@@ -4,6 +4,7 @@
 
  $yourname = $_POST['name'];
  const SAMA = '様';
+ define(TAX, 1.08);//5.2以前の定数定義
 
  print SAMA;
 ?>
@@ -17,14 +18,34 @@ $a = 'hello';//1
 $$a = 'world';
 
 //"は変数の中身を出力することができる
-print "$a;
-print $$a";
+print "$a $$a";
 ?>
 
 <?php
-
 //1で値にした変数の値を変数名にして中身を出力することができる
-print "$a;
-print $$hello";
+print "$a $hello";
+?>
+
+<?php
+//定義済み定数
+//
+print __FILE__;
+print __DIR__;
+\n;
+print __LINE__;
+\n;
+print __DIRECTORY_SEPARATOR__;\n;
+print __PATH_SEPARATOR__;\n;
+print __PHP_VERSION__;\n;
+print __PHP_OS__;\n;
+
+
+
+//8進数 数字0-7までのプレフィックス0
+//16進数 0-9a-fのプレフィックス0xを付ける
+//
+//
+//""中身を展開したい場合,文字列としての''
+//LF(line-feed)
 
 ?>
